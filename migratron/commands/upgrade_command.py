85bbb5cb09979e8acd579c5eb28be7236b6f1df0
fredimatteo <matteofredi.developer@gmail.com>
2025-03-20 14:48:47 +0100

    add upgrade database command

---
+++ migratron/commands/upgrade_command.py
@@ -0,0 +1,12 @@
+from migratron.configuration_parser import load_db_config
+from migratron.databases.services import get_db_connector
+from migratron.migration_engine import MigrationEngine
+
+
+def upgrade_command():
+    db = get_db_connector(load_db_config())
+
+    mg = MigrationEngine(db)
+
+    mg.upgrade()
+
