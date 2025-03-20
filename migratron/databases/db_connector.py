7ed75f49a3754b44c817d55c5887f30d8e82a294
fredimatteo <matteofredi.developer@gmail.com>
2025-03-20 12:04:18 +0100

    add core module and custom logger

--- migratron/databases/db_connector.py
+++ migratron/databases/db_connector.py
@@ -1,8 +1,19 @@
 from abc import ABC, abstractmethod
 
+import psycopg
+
+
 class DatabaseConnector(ABC):
     @abstractmethod
+    def connection(self) -> "psycopg.connection":
+        pass
+
+    @abstractmethod
     def execute(self, query):
+        pass
+
+    @abstractmethod
+    def commit(self):
         pass
 
     @abstractmethod
