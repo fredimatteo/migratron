1557bea6ccb2b103b8dad2c43a0448695812c4d8
fredimatteo <matteofredi.developer@gmail.com>
2025-03-21 08:26:23 +0100

    update migration_engine with const values & downgrade method

--- migratron/migration_engine.py
+++ migratron/migration_engine.py
@@ -1,10 +1,22 @@
-import os
 import uuid
 from io import StringIO
 from pathlib import Path
 from typing import List
 
+from migratron.core.logger import logger
 from migratron.databases.db_connector import DatabaseConnector
+
+
+FIRST_REVISION_ID = '0000'
+UP_PREFIX = "-- Up"
+COMMENT_PREFIX = "--"
+DOWN_PREFIX = "-- Down"
+REVISION_TEMPLATE = [
+    "-- Up migration",
+    "\n",
+    "\n",
+    "-- Down migration"
+]
 
 
 class MigrationEngine:
@@ -15,6 +27,10 @@ class MigrationEngine:
         self.__create_migration_table()
 
     def __create_migration_table(self):
+        """
+        Create the migration table if it does not exist
+        :return: None
+        """
         if self.db:
             self.db.execute("""
                 CREATE TABLE IF NOT EXISTS migrations (
@@ -33,22 +49,16 @@ class MigrationEngine:
         revision_file_path = Path(f"./versions/{revision_file_name}")
 
         with open(revision_file_path, "w") as revision_file:
-            revision_file.writelines([
-                "-- Up migration",
-                "\n",
-                "\n",
-                "-- Down migration"
-            ])
+            revision_file.writelines(REVISION_TEMPLATE)
 
     @staticmethod
     def _get_last_revision_id() -> str:
-        print(os.getcwd())
         folder = Path("./versions")
         file_names: List[str] = [obj.name for obj in folder.iterdir() if obj.is_file()]
 
         file_names_prefix = [file_name.split("_")[0] for file_name in file_names]
         if len(file_names_prefix) == 0:
-            return '0001'
+            return FIRST_REVISION_ID
 
         if len(file_names_prefix) > 0:
             file_names_prefix.sort()
@@ -56,12 +66,17 @@ class MigrationEngine:
 
     @staticmethod
     def _get_last_revision_name() -> str:
-        print(os.getcwd())
         folder = Path("./versions")
         file_names: List[str] = [obj.name for obj in folder.iterdir() if obj.is_file()]
-        return file_names.sort()[-1]
+        file_names.sort()
+        return file_names[-1]
 
     def generate_revision(self, revision_name: str = "") -> None:
+        for char in revision_name:
+            if not char.isalnum() and char != " " and char != "_":
+                raise ValueError("invalid revision name. Only alphanumeric characters, spaces and underscores are allowed")
+
+        revision_name = revision_name.replace(" ", "_")
         if revision_name == "":
             revision_name = str(uuid.uuid4())
 
@@ -74,30 +89,51 @@ class MigrationEngine:
         files_sorted: List[Path] = sorted(files, key=lambda x: x.name.split("_")[0])
         return files_sorted
 
-    def upgrade(self):
+    def upgrade(self) -> None:
         revisions = self._get_all_revisions()
         for revision in revisions:
             lines = revision.read_text().splitlines()
             builder = StringIO()
             for line in lines:
-                if line.startswith("-- Up"):
+                if line.startswith(UP_PREFIX):
                     pass
-                if line.startswith("-- Down"):
+                if line.startswith(DOWN_PREFIX):
                     break
 
-                if not line.startswith("--"):
+                if not line.startswith(COMMENT_PREFIX):
                     builder.write(line)
                     builder.write("\n")
 
-            print(builder.getvalue())
             self.db.execute(builder.getvalue())
             self.db.commit()
 
         last_revision_name = self._get_last_revision_name()
         self.update_migration_table(last_revision_name)
 
-    def update_migration_table(self, revision_name: str):
-        self.db.execute(f"""
-            INSERT INTO migrations (name) VALUES ('{revision_name}')
-        """)
+    def downgrade(self) -> None:
+        revisions = self._get_all_revisions()
+        revisions.reverse()
+        for revision in revisions:
+            lines = revision.read_text().splitlines()
+            builder = StringIO()
+            for line in lines:
+                if line.startswith(DOWN_PREFIX):
+                    pass
+                if line.startswith(UP_PREFIX):
+                    break
+
+                if not line.startswith(COMMENT_PREFIX):
+                    builder.write(line)
+                    builder.write("\n")
+
+            self.db.execute(builder.getvalue())
+            self.db.commit()
+
+        last_revision_name = self._get_last_revision_name()
+        self.update_migration_table(last_revision_name)
+
+    def update_migration_table(self, revision_name: str) -> None:
+        logger.debug('updating migration table with new revision: %s', revision_name)
+        self.db.execute(f"""INSERT INTO migrations (name) VALUES ('{revision_name}')""")
         self.db.commit()
+        logger.debug('migration table updated successfully')
