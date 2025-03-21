3ea09512593ed2bfb12ff959f158c90ca3cf56a3
fredimatteo <matteofredi.developer@gmail.com>
2025-03-21 10:43:20 +0100

    refactor generate_command.py

--- migratron/commands/generate_command.py
+++ migratron/commands/generate_command.py
@@ -5,10 +5,7 @@ from migratron.migration_engine import MigrationEngine
 
 def generate_command(migration_name: str):
     db = get_db_connector(load_db_config())
-
     mg = MigrationEngine(db)
 
     mg.init()
-
     mg.generate_revision(migration_name)
-    print('Migration completed successfully')
