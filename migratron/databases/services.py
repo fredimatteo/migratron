8d6594d5453e102372334892f9a1c2509914d531
fredimatteo <matteofredi.developer@gmail.com>
2025-03-21 08:27:09 +0100

    add get_db_connector utils

---
+++ migratron/databases/services.py
@@ -0,0 +1,16 @@
+from migratron.databases.db_connector import DatabaseConnector
+from migratron.databases.postgres import Postgres
+
+
+def get_db_connector(config: dict) -> DatabaseConnector:
+    db_type = config.get("type", "")
+    if 'postgres' in db_type:
+        return Postgres(
+            host=config["host"],
+            port=config["port"],
+            user=config["user"],
+            password=config["password"],
+            database=config["dbname"]
+        )
+    else:
+        raise ValueError(f"unsupported database type: {db_type}")
