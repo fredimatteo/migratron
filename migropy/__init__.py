796eab0e6755e6275f19936ac395a455579567fd
fredimatteo <matteofredi.developer@gmail.com>
2025-03-24 14:23:13 +0100

    remove CLI usage check from __init__.py

--- migropy/__init__.py
+++ migropy/__init__.py
@@ -1,9 +1,1 @@
-import sys
-
-from migropy.core.logger import logger
-
-if not sys.argv[0].endswith("migropy"):
-    logger.error('this package is intended to be used from CLI only.')
-    sys.exit(1)
-
 current_version = "0.2.0"
