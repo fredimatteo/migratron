0c3495bdd4272c2a54eb6ac807eb01fe632303ee
fredimatteo <matteofredi.developer@gmail.com>
2025-03-28 14:50:16 +0100

    delete deprecated commands modules

--- migropy/commands/list_commands.py
+++
@@ -1,7 +0,0 @@
-from migropy.migration_engine import MigrationEngine
-
-
-def list_command():
-    revisions = MigrationEngine().list_revisions()
-    for r in revisions:
-        print('- ' + r.name)
